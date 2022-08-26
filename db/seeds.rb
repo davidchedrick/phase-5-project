774fba13bbc2115bd18fa4851c13605426a923d6
David Chedrick <davidchedrick@gmail.com>
2022-08-26 11:08:54 -0500

    Models for Posts and Users added

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+user = User.create(username: 'test', password: 'password')
+user.posts.create(title: 'first post', content: 'cutie content!!!')
+user.posts.create(title: 'second post', content: 'love love love')
