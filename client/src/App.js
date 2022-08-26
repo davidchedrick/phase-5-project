671426d0a610172d777f46d66488215709b2fa55
David Chedrick <davidchedrick@gmail.com>
2022-08-26 11:24:41 -0500

    Login/Signup page added

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,41 @@
+import React, { useState, useEffect } from "react";
+import AuthenticatedApp from "./AuthenticatedApp";
+import UnauthenticatedApp from "./UnauthenticatedApp";
+import { BrowserRouter as Router } from "react-router-dom";
+
 function App() {
+    const [currentUser, setCurrentUser] = useState(null);
+    const [authChecked, setAuthChecked] = useState(false);
+
+    useEffect(() => {
+        fetch("/api/me", {
+            credentials: "include",
+        }).then(res => {
+            if (res.ok) {
+                res.json().then(user => {
+                    setCurrentUser(user);
+                    setAuthChecked(true);
+                });
+            } else {
+                setAuthChecked(true);
+            }
+        });
+    }, []);
+
+    if (!authChecked) {
+        return <div></div>;
+    }
     return (
-        <div className="App">
-            <h1>cat!!!!!</h1>
-        </div>
+        <Router>
+            {currentUser ? (
+                <AuthenticatedApp
+                    setCurrentUser={setCurrentUser}
+                    currentUser={currentUser}
+                />
+            ) : (
+                <UnauthenticatedApp setCurrentUser={setCurrentUser} />
+            )}
+        </Router>
     );
 }
 
